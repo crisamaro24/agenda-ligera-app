adc2cb6dd6c22433cbf82c98fac6cb37038ee644
camarogb <camaro@gentlebirth.com>
2021-05-08 12:14:54 -0400

    Crear pagina para seleccion de reportes y corregir la posicion de algunos elementos.

---
+++ COIS6980.AgendaLigera/Services/ReportingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COIS6980.AgendaLigera.Services
+{
+    public interface IReportingService
+    {
+
+    }
+    public class ReportingService
+    {
+    }
+}
