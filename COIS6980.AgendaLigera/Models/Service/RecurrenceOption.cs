99b1faa8fd735a468993bbeb36b35a6830b87043
camarogb <camaro@gentlebirth.com>
2021-05-09 00:48:59 -0400

    Pagina para crear el horario de un servicio.

---
+++ COIS6980.AgendaLigera/Models/Service/RecurrenceOption.cs
@@ -0,0 +1,8 @@
+﻿namespace COIS6980.AgendaLigera.Models.Service
+{
+    public class RecurrenceOption
+    {
+        public int RecurrenceOptionId { get; set; }
+        public string RecurrenceOptionText { get; set; }
+    }
+}
